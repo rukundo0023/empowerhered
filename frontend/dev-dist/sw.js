d46d0587d593873f12e4dc73c9fc71b4cfa81b64
rukundo0023 <r.nshimiyim@alustudent.com>
2025-07-11 09:30:58 +0200

    offline functinalites

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -80,8 +80,14 @@ define(['./workbox-3ad5617a'], (function (workbox) { 'use strict';
   workbox.precacheAndRoute([{
     "url": "registerSW.js",
     "revision": "3ca0b8505b4bec776b69afdba2768812"
+  }, {
+    "url": "index.html",
+    "revision": "0.glf2plh3n48"
   }], {});
   workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
+    allowlist: [/^\/$/]
+  }));
   workbox.registerRoute(/\/api\//, new workbox.NetworkFirst({
     "cacheName": "api-cache",
     "networkTimeoutSeconds": 10,
