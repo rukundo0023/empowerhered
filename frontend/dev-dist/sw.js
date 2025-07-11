d19612033f0d18c0dda4f377f02fb7490cce933e
rukundo0023 <r.nshimiyim@alustudent.com>
2025-07-11 09:13:56 +0200

    offline functionalities

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,11 +81,11 @@ define(['./workbox-3ad5617a'], (function (workbox) { 'use strict';
     "url": "registerSW.js",
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
-    "url": "index.html",
-    "revision": "0.s7dn52vfj4g"
+    "url": "/offline.html",
+    "revision": "0.3o3nbes1ri8"
   }], {});
   workbox.cleanupOutdatedCaches();
-  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/offline.html"), {
     allowlist: [/^\/$/]
   }));
   workbox.registerRoute(/\/api\//, new workbox.NetworkFirst({
