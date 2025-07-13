3ad1017f3408fcf60c4e2eba9a5a99a4ac10049b
rukundo0023 <r.nshimiyim@alustudent.com>
2025-07-13 10:37:32 +0200

    frontend

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -31,7 +31,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 30000, // Increased to 30 seconds
+  timeout: 60000, // Increased to 30 seconds
   withCredentials: true,
 });
 
