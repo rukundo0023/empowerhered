3a1ced1f9ec8937689621d8bcac064df398b826f
rukundo0023 <r.nshimiyim@alustudent.com>
2025-07-20 08:14:18 +0200

    backend

--- backend/index.js
+++ backend/index.js
@@ -55,8 +55,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-// Handle preflight requests explicitly
-app.options('*', cors());
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
